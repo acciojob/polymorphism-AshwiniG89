8f881c1e4bb9af7f64b29d631e60e18b8fefb1a2
Ashwini <pruashu@gmail.com>
2023-01-16 00:49:26 +0530

    Sol

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,21 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args){
+
+        Product p=new Product();
+        p.product();
+
+    }
+
+}
+class Product{
+
+      public int product(int x, int y) {
+
+
+    }
+    public int product(int x, int y, int z) {
+
+    }
 }
